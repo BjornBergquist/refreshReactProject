1d386628e3772475ebbcddafa4afee02b830eaa1
Björn Bergquist <73097586+BjornBergquist@users.noreply.github.com>
2021-07-08 15:41:37 +0200

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {MyFirstComponent} from './components/myfirstcomponent/MyFirstComponent'
-
+import {Routing} from './routes/Routing'
 function App() {
   return (
-    <div>
+    <Routing>
       <h1>React Tutorial</h1>
       <MyFirstComponent name="Björn" age="32"/>
-    </div>
+    </Routing>
   );
 }
 
