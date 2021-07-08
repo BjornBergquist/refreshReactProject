ecd3d08c72a8fb9dc8dae82b6eb39b040795404c
Björn Bergquist <73097586+BjornBergquist@users.noreply.github.com>
2021-07-08 14:59:15 +0200

    created the projects first react component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from 'react'
+import {MyFirstComponent} from './components/myfirstcomponent/MyFirstComponent'
+
 function App() {
   return (
     <div>
       <h1>React Tutorial</h1>
+      <MyFirstComponent />
     </div>
   );
 }
---
+++ src/components/myfirstcomponent/MyFirstComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const MyFirstComponent = () => {
+    return (
+        <React.Fragment>
+            <h1> Det här är min första funktionella reactkomponent. </h1>
+        </React.Fragment>
+    )
+}
