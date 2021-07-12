7466f381a02d3c98b5bbbdce800f5dfdc1067923
Björn Bergquist <73097586+BjornBergquist@users.noreply.github.com>
2021-07-12 12:15:18 +0200

    Added some profile stuff in navbar.

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,14 +1,28 @@
 import React, {useContext} from 'react'
+import { useHistory } from 'react-router-dom';
 import { UserContext } from '../../shared/global/provider/UserProvider'
 import "./Profile.css"
 
 export const Profile = () => {
     const [authenticatedUser, setAuthenticatedUser] = useContext(UserContext);
+    const history = useHistory();
+
+    const logout = () => {
+        localStorage.removeItem("username")
+        setAuthenticatedUser(false)
+        history.push("/")
+    }
 
     return (
         <div className="profile__wrapper">
             <img className="profile__img" src={"https://www.thispersondoesnotexist.com/image"} alt="" />
             <span className="profile__username">{authenticatedUser}</span>
+            <div className="profile__dropdown">
+                <a onClick={() => history.push("/settings")}>Settings</a>
+                <a onClick={() => history.push("/profile")}>Profile</a>
+                <span className="break"/>
+                <a onClick={() => logout()}>Log Out</a>
+            </div>
         </div>
     )
 }
