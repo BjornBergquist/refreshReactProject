2470c77ed1e7184fa284603d57507e8578c9fdec
Björn Bergquist <73097586+BjornBergquist@users.noreply.github.com>
2021-07-12 10:25:36 +0200

    Created a useEffect Hook

--- src/view/HomeView.js
+++ src/view/HomeView.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 export const HomeView = () => {
+    const [count, setCount] = useState(0)
+    useEffect(() => {
+        alert("Component is being rendered.")
+        return () =>  {
+            alert("Component is being removed.")
+        }
+    }, [count])
     return (
         <div>
             <h1>This is the HomeView</h1>
+            <h2>{count}</h2>
+            <button onClick={() => setCount(count + 1)}>Add 1</button>
         </div>
     )
 }
